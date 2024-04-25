f457d854325a58e8a20321eedc884d355f758848
krashyd <a52441@alunos.ipb.pt>
2024-04-25 18:28:48 +0100

    Modify the cube.cs script
    a52441

--- Assets/ModTheCube/Cube.cs
+++ Assets/ModTheCube/Cube.cs
@@ -5,19 +5,30 @@ using UnityEngine;
 public class Cube : MonoBehaviour
 {
     public MeshRenderer Renderer;
+    private float horizontalInput;
+    private float verticalInput;
+    public float speed = 45;
     
     void Start()
     {
-        transform.position = new Vector3(3, 4, 1);
-        transform.localScale = Vector3.one * 1.3f;
+        transform.position = new Vector3(0, 7, 2);
+        transform.localScale = Vector3.one * 12f;
         
         Material material = Renderer.material;
         
-        material.color = new Color(0.5f, 1.0f, 0.3f, 0.4f);
+        material.color = new Color(0.8f, 1.0f, 0.3f, 0.2f);
     }
     
     void Update()
     {
-        transform.Rotate(10.0f * Time.deltaTime, 0.0f, 0.0f);
+        transform.Rotate(90.0f * Time.deltaTime, 75.0f * Time.deltaTime, 80.0f * Time.deltaTime);
+
+        //input player horizontal
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        //input player vertical
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
     }
 }
